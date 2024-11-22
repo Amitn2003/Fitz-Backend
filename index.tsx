68a49ce50bc6ffc2cf17696f6d48bb86a79bb9e8
Amit Naskar <amitnaskar2003@gmail.com>
2024-11-23 00:30:47 +0530

    Index.tsx deleted

--- index.tsx
+++
@@ -1,6 +0,0 @@
-mkdir fitness-app-backend
-cd fitness-app-backend
-npm init -y
-npm install express mongoose bcrypt jsonwebtoken dotenv zod express-rate-limit cors helmet
-npm install --save-dev nodemon
-
